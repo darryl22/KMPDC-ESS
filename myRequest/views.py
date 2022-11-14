95980399a2f9700bb2fb958f2602d3062847f214
enockabere <maebaenock95@gmail.com>
2022-11-14 16:48:33 +0300

    deploy changes

--- myRequest/views.py
+++ myRequest/views.py
@@ -8,14 +8,28 @@ class UserObjectMixins(object):
     model =None
     session = requests.Session()
     session.auth = config.AUTHS
-    todays_date = dt.datetime.now().strftime("%b. %d, %Y %A")
 
     def get_object(self,endpoint):
         response = self.session.get(endpoint, timeout=10).json()
         return response
+    
+    def one_filter(self,endpoint,property,filter,field_name):
 
-    def get_filtered_data(self,endpoint,property,filter):
-        Access_Point = config.O_DATA.format(f"{endpoint}?$filter={property}%20eq%20%27{filter}%27")
+        Access_Point = config.O_DATA.format(f"{endpoint}?$filter={property}%20{filter}%20%27{field_name}%27")
+        response = self.get_object(Access_Point)['value']
+        count=len(response)
+        return count,response
+   
+    def double_filtered_data(self,endpoint,property_x,filter_x,filed_name_x,operater_1,property_y,filter_y,field_name_y):
 
-        response = self.get_object(Access_Point)
-        return response
+        Access_Point = config.O_DATA.format(f"{endpoint}?$filter={property_x}%20{filter_x}%20%27{filed_name_x}%27%20{operater_1}%20{property_y}%20{filter_y}%20%27{field_name_y}%27")
+        response = self.get_object(Access_Point)['value']
+        count=len(response)
+        return count,response
+
+    def triple_filtered_data(self,endpoint,property_x,filter_x,filed_name_x,operater_1,property_y,filter_y,field_name_y,operater_2,property_z,filter_z,field_name_z):
+
+        Access_Point = config.O_DATA.format(f"{endpoint}?$filter={property_x}%20{filter_x}%20%27{filed_name_x}%27%20{operater_1}%20{property_y}%20{filter_y}%20%27{field_name_y}%27%20{operater_2}%20{property_z}%20{filter_z}%20%27{field_name_z}%27")
+        response = self.get_object(Access_Point)['value']
+        count=len(response)
+        return count,response
