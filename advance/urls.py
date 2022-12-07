6ea9b3bc2ae2b06cf29fd4d25213dae788909000
enockabere <maebaenock95@gmail.com>
2022-12-07 15:08:45 +0300

    advance chnages

--- advance/urls.py
+++ advance/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     path("salary advance", views.advance.as_view(),name="advance"),
     path("advanceDetail/<str:pk>",views.advanceDetail.as_view(), name="advanceDetail"),
-    path("FnRequestSalaryAdvanceApproval/<str:pk>",views.FnRequestSalaryAdvanceApproval, name="FnRequestSalaryAdvanceApproval"),
-    path("FnCancelSalaryAdvanceApproval/<str:pk>",views.FnCancelSalaryAdvanceApproval, name="FnCancelSalaryAdvanceApproval"),
-    path("UploadAdvanceAttachment/<str:pk>",views.UploadAdvanceAttachment, name="UploadAdvanceAttachment"),
-    path("DeleteAdvanceAttachment/<str:pk>",views.DeleteAdvanceAttachment, name="DeleteAdvanceAttachment"),
+    path("FnRequestSalaryAdvanceApproval/<str:pk>",views.FnRequestSalaryAdvanceApproval.as_view(), name="FnRequestSalaryAdvanceApproval"),
+    path("FnCancelSalaryAdvanceApproval/<str:pk>",views.FnCancelSalaryAdvanceApproval.as_view(), name="FnCancelSalaryAdvanceApproval"),
+    path("UploadAdvanceAttachment/<str:pk>",views.UploadAdvanceAttachment.as_view(), name="UploadAdvanceAttachment"),
+    path("DeleteAdvanceAttachment/<str:pk>",views.DeleteAdvanceAttachment.as_view(), name="DeleteAdvanceAttachment"),
 ]
--- advance/views.py
+++ advance/views.py
@@ -4,44 +4,25 @@ from django.conf import settings as config
 import json
 from django.contrib import messages
 import datetime as dt
-from requests.auth import HTTPBasicAuth
-from zeep import Client
-from zeep.transports import Transport
-from requests import Session
 from django.views import View
+from myRequest.views import UserObjectMixins
 import base64
 
-# Create your views here.
-class UserObjectMixin(object):
-    model =None
-    session = requests.Session()
-    session.auth = config.AUTHS
-    todays_date = dt.datetime.now().strftime("%b. %d, %Y %A")
-    def get_object(self,endpoint):
-        response = self.session.get(endpoint, timeout=10).json()
-        return response
-        
-class advance(UserObjectMixin,View):
+class advance(UserObjectMixins,View):
     def get(self,request):
         try:
             fullname =  request.session['User_ID']
             year = request.session['years']
             empNo =request.session['Employee_No_']
 
-            Access_Point = config.O_DATA.format(f"/QySalaryAdvances?$filter=Employee_No%20eq%20%27{empNo}%27")
-            response = self.get_object(Access_Point)
-            openAdvance = [x for x in response['value'] if x['Loan_Status'] == 'Application']
-            Pending = [x for x in response['value'] if x['Loan_Status'] == 'Being Processed']
-            Approved = [x for x in response['value'] if x['Loan_Status'] == 'Approved']
+            response = self.one_filter("/QySalaryAdvances","Employee_No","eq",empNo)
+            openAdvance = [x for x in response[1] if x['Loan_Status'] == 'Application']
+            Pending = [x for x in response[1] if x['Loan_Status'] == 'Being Processed']
+            Approved = [x for x in response[1] if x['Loan_Status'] == 'Approved']
 
             SalaryProducts = config.O_DATA.format("/QyLoanProductTypes")
             SalaryResponse = self.get_object(SalaryProducts)
             salary = SalaryResponse['value']
-
-            counts = len(openAdvance)
-            pend = len(Pending)
-            counter = len(Approved)
-
         except requests.exceptions.RequestException as e:
             print(e)
             messages.info(request, e)
@@ -54,11 +35,12 @@ class advance(UserObjectMixin,View):
             print(e)
             messages.error(request,e)
             return redirect('auth')
-        ctx = {"today": self.todays_date, "res": openAdvance,
-            "count": counts, "response": Approved,
-            "counter": counter,"pend": pend,
+        ctx = {
+            "today": self.todays_date, "res": openAdvance,
+            "response": Approved,
             "pending": Pending, "year": year,
-            "full": fullname,"salary":salary}
+            "full": fullname,"salary":salary
+            }
         return render(request,"advance.html",ctx)
     def post(self, request):
         if request.method == "POST":
@@ -70,55 +52,45 @@ class advance(UserObjectMixin,View):
                 myUserId = request.session['User_ID']
                 installments = int(request.POST.get('installments'))
                 myAction = request.POST.get('myAction')
-            except ValueError as e:
-                print(e)
-                messages.error(request,e)
-                return redirect('advance')
-            except KeyError as e:
-                print(e)
-                messages.info(request, "Session Expired. Please Login")
-                return redirect('auth')
-            if installments <=0 or installments > 12:
-                messages.info(request, "Installments cannot be less than 1 or more than 12")
-                return redirect('advance')
 
-            try:
-                response = config.CLIENT.service.FnSalaryAdvanceApplication(
+                if installments <=0 or installments > 12:
+                    messages.info(request, "Installments cannot be less than 1 or more than 12")
+                    return redirect('advance')
+
+                response = self.zeep_client(request).service.FnSalaryAdvanceApplication(
                     loanNo, employeeNo,productType,amountRequested,myUserId,installments, myAction)
-                print(response)
+
                 if response == True:
                     messages.success(request, "Request Successful")
                     return redirect('advance')
-            
+                messages.error(request, response)
+                return redirect('advance')
             except Exception as e:
                 messages.error(request, e)
                 print(e)
         return redirect('advance')
 
-class advanceDetail(UserObjectMixin,View):
+class advanceDetail(UserObjectMixins,View):
     def get(self, request,pk):
         try:
             fullname = request.session['User_ID']
             year = request.session['years']
             empNo =request.session['Employee_No_']
 
-            Access_Point = config.O_DATA.format(f"/QySalaryAdvances?$filter=Employee_No%20eq%20%27{empNo}%27%20and%20Loan_No%20eq%20%27{pk}%27")
-            response = self.get_object(Access_Point)
-            for advance in response['value']:
+            response = self.double_filtered_data("/QySalaryAdvances","Loan_No", "eq",pk,
+                            "and","Employee_No","eq",empNo)
+            for advance in response[1]:
                 res = advance
                 state = advance['Loan_Status']
 
-            Approver = config.O_DATA.format(f"/QyApprovalEntries?$filter=Document_No_%20eq%20%27{pk}%27")
-            res_approver = self.get_object(Approver)
-            Approvers = [x for x in res_approver['value']]
+            res_approver = self.one_filter("/QyApprovalEntries","Document_No_","eq",pk)
+            Approvers = [x for x in res_approver[1]]
 
-            RejectComments = config.O_DATA.format(f"/QyApprovalCommentLines?$filter=Document_No_%20eq%20%27{pk}%27")
-            RejectedResponse = self.get_object(RejectComments)
-            Comments = [x for x in RejectedResponse['value']]
+            RejectedResponse = self.one_filter("/QyApprovalCommentLines","Document_No_","eq",pk)
+            Comments = [x for x in RejectedResponse[1]]
 
-            Access_File = config.O_DATA.format(f"/QyDocumentAttachments?$filter=No_%20eq%20%27{pk}%27")
-            res_file = self.get_object(Access_File)
-            allFiles = [x for x in res_file['value']]
+            res_file = self.one_filter("/QyDocumentAttachments","No_","eq",pk)
+            allFiles = [x for x in res_file[1]]
 
         except requests.exceptions.ConnectionError as e:
             print(e)
@@ -131,89 +103,84 @@ class advanceDetail(UserObjectMixin,View):
         except Exception as e:
                 messages.error(request, e)
                 print(e)
-        ctx = {"today": self.todays_date, "res": res,
-                "Approvers": Approvers, "state": state,"file":allFiles,
-                "year": year, "full": fullname,"Comments":Comments}
+        ctx = {
+            "today": self.todays_date, "res": res,
+            "Approvers": Approvers, "state": state,"file":allFiles,
+            "year": year, "full": fullname,"Comments":Comments
+            }
 
         return render(request,"advanceDetails.html",ctx)
 
-def FnRequestSalaryAdvanceApproval(request,pk):
-    Username = request.session['User_ID']
-    Password = request.session['password']
-    AUTHS = Session()
-    AUTHS.auth = HTTPBasicAuth(Username, Password)
-    CLIENT = Client(config.BASE_URL, transport=Transport(session=AUTHS))
-    if request.method == "POST":
-        try:
-            response = CLIENT.service.FnRequestSalaryAdvanceApproval(
-                request.session['Employee_No_'],pk)
-            print(response)
-            if response == True:
-                messages.success(request, "Approval Request Sent Successfully ")
-                return redirect('advance')
-            messages.error(request, response)
-            return redirect('advanceDetail', pk=pk)
-        except Exception as e:
-            messages.error(request, e)
-            print(e) 
-            return redirect('advanceDetail', pk=pk)       
-    return redirect('advanceDetail', pk=pk)
-
-def FnCancelSalaryAdvanceApproval(request,pk):
-    Username = request.session['User_ID']
-    Password = request.session['password']
-    AUTHS = Session()
-    AUTHS.auth = HTTPBasicAuth(Username, Password)
-    CLIENT = Client(config.BASE_URL, transport=Transport(session=AUTHS))
-    if request.method == "POST":
-        try:
-            response = CLIENT.service.FnCancelSalaryAdvanceApproval(
-                request.session['Employee_No_'],pk)
-            print(response)
-            if response == True:
-                messages.success(request, "Approval Request Sent Successfully ")
-                return redirect('advance')
-        except Exception as e:
-            messages.error(request, e)
-            print(e)        
-    return redirect('advanceDetail', pk=pk)
-
-def UploadAdvanceAttachment(request, pk):
-    if request.method == "POST":
-        try:
-            tableID = 52177630
-            attach = request.FILES.getlist('attachment')
-        except Exception as e:
-            return redirect('advanceDetail', pk=pk)
-        for files in attach:
-            fileName = request.FILES['attachment'].name
-            attachment = base64.b64encode(files.read())
+class FnRequestSalaryAdvanceApproval(UserObjectMixins,View):
+    def post(self,request,pk):
+        if request.method == "POST":
             try:
-                response = config.CLIENT.service.FnUploadAttachedDocument(
-                    pk, fileName, attachment, tableID,request.session['User_ID'])
+                response = self.zeep_client(request).service.FnRequestSalaryAdvanceApproval(
+                    request.session['Employee_No_'],pk)
+                print(response)
+                if response == True:
+                    messages.success(request, "Approval Request Sent Successfully ")
+                    return redirect('advanceDetail', pk=pk) 
+                messages.error(request, response)
+                return redirect('advanceDetail', pk=pk)
+            except Exception as e:
+                messages.error(request, e)
+                print(e) 
+                return redirect('advanceDetail', pk=pk)       
+        return redirect('advanceDetail', pk=pk)
+
+class FnCancelSalaryAdvanceApproval(UserObjectMixins,View):
+    def post(self,request,pk):
+        if request.method == "POST":
+            try:
+                response = self.zeep_client(request).service.FnCancelSalaryAdvanceApproval(
+                    request.session['Employee_No_'],pk)
+                if response == True:
+                    messages.success(request, "Approval Request Sent Successfully")
+                    return redirect('advanceDetail', pk=pk)
+                messages.errors(request, response)
+                return redirect('advanceDetail', pk=pk) 
+            except Exception as e:
+                messages.error(request, e)
+                print(e)        
+        return redirect('advanceDetail', pk=pk)
+
+class UploadAdvanceAttachment(UserObjectMixins, View):
+    def post(self,request,pk):
+        if request.method == "POST":
+            try:
+                tableID = 52177630
+                attach = request.FILES.getlist('attachment')
+                for files in attach:
+                    fileName = request.FILES['attachment'].name
+                    attachment = base64.b64encode(files.read())
+                    response = config.CLIENT.service.FnUploadAttachedDocument(
+                            pk, fileName, attachment, tableID,request.session['User_ID'])
+                if response == True:
+                    messages.success(request, "File(s) Upload Successful")
+                    return redirect('advanceDetail', pk=pk)
+                messages.error(request, "Failed, Try Again")
+                return redirect('advanceDetail', pk=pk)
             except Exception as e:
                 messages.error(request, e)
                 print(e)
-        if response == True:
-            messages.success(request, "File(s) Upload Successful")
-            return redirect('advanceDetail', pk=pk)
-        else:
-            messages.error(request, "Failed, Try Again")
-            return redirect('advanceDetail', pk=pk)
-    return redirect('advanceDetail', pk=pk)
+        return redirect('advanceDetail', pk=pk)
 
-def DeleteAdvanceAttachment(request,pk):
-    if request.method == "POST":
-        docID = int(request.POST.get('docID'))
-        tableID= int(request.POST.get('tableID'))
-        try:
-            response = config.CLIENT.service.FnDeleteDocumentAttachment(
-                pk,docID,tableID)
-            print(response)
-            if response == True:
-                messages.success(request, "Deleted Successfully")
+class DeleteAdvanceAttachment(UserObjectMixins,View):
+    def post(self,request,pk):
+        if request.method == "POST":
+            try:
+                docID = int(request.POST.get('docID'))
+                tableID= int(request.POST.get('tableID'))
+                response = config.CLIENT.service.FnDeleteDocumentAttachment(
+                    pk,docID,tableID)
+
+                if response == True:
+                    messages.success(request, "Deleted Successfully")
+                    return redirect('advanceDetail', pk=pk)
+                messages.error(request, response)
                 return redirect('advanceDetail', pk=pk)
-        except Exception as e:
-            messages.error(request, e)
-            print(e)
-    return redirect('advanceDetail', pk=pk)
+            except Exception as e:
+                messages.error(request, e)
+                print(e)
+        return redirect('advanceDetail', pk=pk)
