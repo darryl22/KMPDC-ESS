f5b5822690cfefaa8df69a3c1c46aa5357edc431
enockabere <maebaenock95@gmail.com>
2023-01-04 11:41:04 +0300

    inherit css

--- KMPDC/settings.py
+++ KMPDC/settings.py
@@ -1,8 +1,6 @@
 
 from pathlib import Path
 import os
-import django_heroku
-import dj_database_url
 from decouple import config, Csv
 from requests import Session
 from requests_ntlm import HttpNtlmAuth
